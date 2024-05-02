e3a30bfd6a9a2021bbfc3fd5719f86e80114963b
Burke Landon <128099556+Burke-LJX@users.noreply.github.com>
2024-05-02 14:12:34 -0400

    Update login.java
    updated imports

--- welcome_screen/login.java
+++ welcome_screen/login.java
@@ -4,6 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
